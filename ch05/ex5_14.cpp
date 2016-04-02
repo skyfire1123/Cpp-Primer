4d94f3b79055705a3dfd94267554356a466a8aab
skyfire1123 <574579210@qq.com>
2016-04-02 15:30:59 +0800

    Update ex5_14.cpp
    A junior virsion

--- ch05/ex5_14.cpp
+++ ch05/ex5_14.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 #include <string>
 
-using std::cout; using std::cin; using std::endl; using std::string; using std::pair;
-
+using std::cin; using std::cout; using std::endl;using std::string;
 int main()
-{ 
-    pair<string, int> max_duplicated;
-    int count = 0;
-    for (string str, prestr; cin >> str; prestr = str)
-    {
-        if (str == prestr) ++count;
-        else count = 0; 
-        if (count > max_duplicated.second) max_duplicated = { prestr, count };
-    }
-    
-    if (max_duplicated.first.empty()) cout << "There's no duplicated string." << endl;
-    else cout << "the word " << max_duplicated.first << " occurred " << max_duplicated.second + 1 << " times. " << endl;
-    
-    return 0;
+{
+	string prestr,nowstr,tempstr,maxstr;
+	int cnt = 1,maxcnt = 1;
+	while(cin >> tempstr){
+		nowstr = tempstr;
+		if(nowstr == prestr){
+			++cnt;
+		}else{
+			if(cnt > maxcnt){
+				maxcnt = cnt;
+				maxstr = prestr;
+			}
+			prestr = nowstr;
+			cnt = 1;
+		}
+	}
+	if(cnt > maxcnt){              //in case of that the last word repeats the most times
+		maxcnt = cnt;
+		maxstr = prestr;
+	}
+	if(maxcnt > 1){
+		cout << maxstr << " repeated " << maxcnt << " times." << endl;
+	}else{
+		cout << "No word repeated." << endl;
+	}
+	return 0;
 }
